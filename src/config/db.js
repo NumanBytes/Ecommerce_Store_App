4e7acc66cc76a5ad7c99b58a40146b47770bc0f1
Numan <numanrazzaq350@gmail.com>
2023-10-01 05:21:19 +0500

    Final Commit

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = () => {
+    const uri = process.env.MONGODB_CONNECTION_URI;
+    // console.log(uri)
+    mongoose.connect(uri,{
+        autoCreate: true,
+        autoIndex: true
+    }).then((res)=>{
+        console.log("DB Connected Successfully")
+    }).catch((err)=>{
+        console.log("Alas! DB Connection Error");
+    })
+}
+
+export default connectDB;
